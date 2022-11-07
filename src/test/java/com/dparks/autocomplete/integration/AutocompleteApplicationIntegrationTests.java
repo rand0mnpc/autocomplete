8988e245d15675e0f5aee533ff2c5f3f1cf76921
randomnpc <dparks1996@gmail.com>
2022-11-06 21:33:42 -0800

    Changed AutocompleteApplicationInt egrationTests to take advantage of JUnit 5 @ParametrizedTest

--- src/test/java/com/dparks/autocomplete/integration/AutocompleteApplicationIntegrationTests.java
+++ src/test/java/com/dparks/autocomplete/integration/AutocompleteApplicationIntegrationTests.java
@@ -2,18 +2,21 @@ package com.dparks.autocomplete.integration;
 
 import com.dparks.autocomplete.application.AutocompleteApplication;
 import com.dparks.autocomplete.model.AutocompleteResponse;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.assertj.core.api.Assertions;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.stream.Stream;
 
 @SpringBootTest(classes = AutocompleteApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AutocompleteApplicationIntegrationTests {
+
+    private static final String ENDPOINT_FORMAT_STRING = "http://localhost:%s/autocomplete?inputText=%s";
 
     @LocalServerPort
     private int port;
@@ -21,25 +24,25 @@ public class AutocompleteApplicationIntegrationTests {
     @Autowired
     TestRestTemplate testRestTemplate;
 
-    @Test
-    public void testAutocomplete() {
-        // setup
-        Map<String, String> inputToExpectedMap = new HashMap<>();
-        inputToExpectedMap.put("absorpta", "absorptance");
-        inputToExpectedMap.put("accomplet", "accompletive");
-        inputToExpectedMap.put("toyt", "toytown");
-        inputToExpectedMap.put("0", null);
-        inputToExpectedMap.put("", null);
-        String endpointFormatString = "http://localhost:%s/autocomplete?inputText=%s";
-
+    @ParameterizedTest
+    @MethodSource("provideParameters")
+    public void testAutocompleteParametrized(String prefix, String expected) {
         // trigger & verify
-        inputToExpectedMap.forEach((input, expected) -> {
-            String actual = getForObject(endpointFormatString, input).getPredictedText();
-            Assertions.assertThat(actual).isEqualTo(expected);
-        });
+        String actual = getForObject(prefix).getPredictedText();
+        Assertions.assertThat(actual).isEqualTo(expected);
     }
 
-    private AutocompleteResponse getForObject(String endpointFormatString, String input) {
-        return testRestTemplate.getForObject(String.format(endpointFormatString, port, input), AutocompleteResponse.class);
+    private AutocompleteResponse getForObject(String input) {
+        return testRestTemplate.getForObject(
+                String.format(AutocompleteApplicationIntegrationTests.ENDPOINT_FORMAT_STRING, port, input),
+                AutocompleteResponse.class);
+    }
+
+    private static Stream<Arguments> provideParameters() {
+        return Stream.of(
+                Arguments.of("absorpta", "absorptance"),
+                Arguments.of("accomplet", "accompletive"),
+                Arguments.of("toyt", "toytown"),
+                Arguments.of("0", null));
     }
 }
